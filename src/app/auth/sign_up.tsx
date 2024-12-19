56c21cebca7cb45989f21fca278359a468edcd60
inumaru109 <189873245+inumaru109@users.noreply.github.com>
2024-12-20 00:11:55 +0900

    Create sign up function

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,15 +1,27 @@
 import {
     View, Text, StyleSheet,
-    TouchableOpacity, TextInput
+    TouchableOpacity, TextInput, Alert
 } from 'react-native'
 import { useState } from 'react'
-
 import { Link, router } from 'expo-router'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+import { auth } from '../../config'
 import Button from '../../components/Button'
 
-const handlePress = (): void => {
+const handlePress = (email: string, password: string): void => {
     //会員登録
-    router.push('/memo/list')
+    console.log(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential.user.uid)
+            router.replace('/memo/list')
+        })
+        .catch((error) => {
+            const { code, message } = error
+            console.log(code, message)
+            Alert.alert(message)
+        })
 }
 
 const SignUp = (): JSX.Element => {
@@ -37,7 +49,7 @@ const SignUp = (): JSX.Element => {
                     placeholder='Password'
                     textContentType='password'
                 />
-                <Button label='submit' onPress={handlePress} />
+                <Button label='submit' onPress={() => { handlePress(email, password) }} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
                     <Link href='/auth/log_in' asChild>
