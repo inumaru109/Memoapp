a3b09a7c110cadf06c83e3ea299111f102d7226a
inumaru109 <189873245+inumaru109@users.noreply.github.com>
2024-12-22 00:50:10 +0900

    Creation of log_in/log_out function is completed

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,16 +1,26 @@
 import {
     View, Text, StyleSheet,
-    TouchableOpacity, TextInput
+    TouchableOpacity, TextInput, Alert
 } from 'react-native'
 
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
 import Button from '../../components/Button'
+import { auth } from '../../config'
 
-const handlePress = (): void => {
-    //ログイン
-    router.replace('/memo/list')
+const handlePress = (email: string, password: string): void => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential.user.uid)
+            router.replace('/memo/list')
+        })
+        .catch((error) => {
+            const { code, message } = error
+            console.log(code, message)
+            Alert.alert(message)
+        })
 }
 
 const LogIn = (): JSX.Element => {
@@ -38,10 +48,10 @@ const LogIn = (): JSX.Element => {
                     placeholder='Password'
                     textContentType='password'
                 />
-                <Button label='submit' onPress={handlePress} />
+                <Button label='submit' onPress={() => { handlePress(email, password) }} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not registered?</Text>
-                    <Link href='/auth/sign_up' asChild>
+                    <Link href='/auth/sign_up' asChild replace>
                         <TouchableOpacity>
                             <Text style={styles.footerLink}>Sine up here</Text>
                         </TouchableOpacity>
