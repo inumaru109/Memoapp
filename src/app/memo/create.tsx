f48c49ee8f54d7ac62350c7b5ef8ce9eb372896d
inumaru109 <189873245+inumaru109@users.noreply.github.com>
2024-12-22 09:48:14 +0900

    Add function of saving memo to DB

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -2,21 +2,42 @@ import {
     View, TextInput, StyleSheet, KeyboardAvoidingView
 } from 'react-native'
 import { router } from 'expo-router'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
+import { auth, db } from '../../config'
 
-const handlePress = (): void => {
-    router.back()
+const handlePress = (bodyText: string): void => {
+    if (auth.currentUser === null) { return }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    addDoc(ref, {
+        bodyText,
+        updatedAd: Timestamp.fromDate(new Date())
+    })
+        .then((docRef) => {
+            console.log('success', docRef.id)
+            router.back()
+        })
+        .catch(() => {
+            console.log('error')
+        })
 }
 
 const Create = () => {
+    const [bodyText, setBodyText] = useState('')
     return (
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value='' />
+                <TextInput
+                    multiline
+                    style={styles.input}
+                    value={bodyText}
+                    onChangeText={(text) => setBodyText(text)}
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => { handlePress(bodyText) }}>
                 <Icon name='check' size={40} color='#FFFFFF' />
             </CircleButton>
         </KeyboardAvoidingView >
